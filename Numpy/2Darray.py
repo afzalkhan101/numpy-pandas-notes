3013c3d8f96ab600f7106c6401dcd80d3071748a
Afzal Khan <afzalkhan101.contact@gmail.com>
2025-11-16 08:23:01 +0600

    updated

---
+++ Numpy/2Darray.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr2d = np.array([[1,3,3],[2,4,5]])
+print(arr2d.shape) ## shape  of array 2d like 2*3 Mettrix
+print(arr2d.size)  ##Total element of array
+print(arr2d.ndim) ## Number of array Dimention
+print(arr2d.ndim)
+print(arr2d.ndim)
+
+
